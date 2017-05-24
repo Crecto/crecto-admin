3af4d58c8b67f3aca5050a540132a456b7a9c829
Nick Franken <shnick@gmail.com>
2017-05-23 20:18:24 -0500

    pagination, fixed pkey references

--- src/CrectoAdmin.cr
+++ src/CrectoAdmin.cr
@@ -36,7 +36,7 @@ def self.admin_resource(model : Crecto::Model.class, repo, **opts)
   collection_attributes = model.responds_to?(:collection_attributes) ? model.collection_attributes :  model.fields.map{|f| f[:name] }
   show_page_attributes = model.responds_to?(:show_page_attributes) ? model.show_page_attributes :  model.fields.map{|f| f[:name] }
   form_attributes = model.responds_to?(:form_attributes) ? model.form_attributes :  model.fields.map{|f| f[:name] }
-
+  per_page = 20
   resource = {
     model: model,
     repo: repo,
@@ -48,9 +48,9 @@ def self.admin_resource(model : Crecto::Model.class, repo, **opts)
 
   get "/admin/#{model.table_name}" do |ctx|
     offset = ctx.params.query["offset"]? ? ctx.params.query["offset"].to_i : 0
-    query = Crecto::Repo::Query.limit(20).offset(offset)
+    query = Crecto::Repo::Query.limit(per_page).offset(offset)
     data = repo.all(model, query)
-    count = repo.aggregate(model, :count, :id)
+    count = repo.aggregate(model, :count, resource[:model].primary_key_field_symbol).as(Int64)
     render "src/views/index.ecr", "src/views/admin_layout.ecr"
   end
 
@@ -63,7 +63,7 @@ def self.admin_resource(model : Crecto::Model.class, repo, **opts)
       end
       item.update_from_hash(query_hash)
       repo.update(item)
-      ctx.redirect "/admin/#{model.table_name}/#{item.id}"
+      ctx.redirect "/admin/#{model.table_name}/#{item.pkey_value}"
     else
       item = repo.get!(model, ctx.params.url["id"])
       render "src/views/show.ecr", "src/views/admin_layout.ecr"
